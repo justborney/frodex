b72a076404da6029d528fa95a84131a16bd961c6
just born <justborney@gmail.com>
2023-12-20 18:57:55 +0500

    fix fetch

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -1,10 +1,10 @@
 export const get_posts = async () => {
-    const response = await fetch('http://localhost:8001/api/likes');
+    const response = await fetch(`http://${document.location.hostname}:8001/api/likes`);
     return response.json();
 }
 
 export const post_like = async (uuid) => {
-    return await fetch('http://localhost:8001/api/likes', {
+    return await fetch(`http://${document.location.hostname}:8001/api/likes`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
